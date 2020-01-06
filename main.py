80946ce6600dd2b957ebe0c535b1bffeef514c9c
greg19735 <greg19735@gmail.com>
2020-01-06 13:30:48 -0500

    jan 6 test

--- main.py
+++ main.py
@@ -11,16 +11,17 @@ internetSites = [WebsiteObj("https://www.epa.gov/faca",'faca',"https://www.epa.g
                WebsiteObj("https://www.epa.gov/nscep",'nscep',"https://www.epa.gov", "/nscep", ".txt", "internet", True),
                WebsiteObj("https://www.epa.gov/greeningepa", 'greeningepa', "https://www.epa.gov", "/greeningepa", ".txt", "internet", True) ]
 
-intranetSites = [WebsiteObj("https://intranet.epa.gov/oarm/orbo/index.html",'orom',"https://intranet.epa.gov", "/oarm/orbo", ".txt", "intranet", False),
-               WebsiteObj("https://intranet.epa.gov/ogd/",'ogd',"https://intranet.epa.gov", "/ogd", ".txt", "intranet", False),
-               WebsiteObj("https://intranet.epa.gov/oarm/",'oarm',"https://intranet.epa.gov", "/oarm", ".txt", "intranet", False),
-               WebsiteObj("https://intranet.epa.gov/oa/",'oa',"https://intranet.epa.gov", "/oa", ".txt", "intranet", False),
-               WebsiteObj("https://intranet.epa.gov/ohr/",'ohr',"https://intranet.epa.gov", "/ohr", ".txt", "intranet", False),
+intranetSites = [WebsiteObj("https://intranet.epa.gov/oarm/orbo/index.html",'orom',"https://intranet.epa.gov", "/oarm/orbo", ".txt", "intranet", True),
+               WebsiteObj("https://intranet.epa.gov/ogd/",'ogd',"https://intranet.epa.gov", "/ogd", ".txt", "intranet", True),
+               WebsiteObj("https://intranet.epa.gov/oarm/",'oarm',"https://intranet.epa.gov", "/oarm", ".txt", "intranet", True),
+               WebsiteObj("https://intranet.epa.gov/oa/",'oa',"https://intranet.epa.gov", "/oa", ".txt", "intranet", True),
+               WebsiteObj("https://intranet.epa.gov/ohr/",'ohr',"https://intranet.epa.gov", "/ohr", ".txt", "intranet", True),
+               WebsiteObj("https://intranet.epa.gov/oarm/",'ohr',"https://intranet.epa.gov", "/oarm", ".txt", "intranet", True),
                WebsiteObj("https://contracts.epa.gov/",'contracts',"https://contracts.epa.gov", "", ".txt", "intranet", False),
                WebsiteObj("https://oamintra.epa.gov/node/27",'oamintra',"https://oamintra.epa.gov", "", ".txt", "intranet", False),
                WebsiteObj("https://www.epa.gov/greeningepa", 'greeningepa', "https://www.epa.gov", "/greeningepa", ".txt", "intranet", False) ]
 
-allsites = internetSites + intranetSites
+allsites = intranetSites
 
 
 for site in allsites:
